0128b97cf3b3565ccdaab7b65947db5b2e5b8b4e
thehancode <the.hancode1@gmail.com>
2024-11-08 16:34:29 -0500

    accept full link

--- script.js
+++ script.js
@@ -2,15 +2,22 @@
 const videoInput = document.getElementById('videoInput');
 const videoIframe = document.querySelector('iframe');
 
+function getYouTubeVideoCode(url) {
+  // Use a regular expression to match the last part of the URL that isn't a query parameter
+  const match = url.match(/(?:\/|v=|\/shorts\/)([a-zA-Z0-9_-]{11})(?:\?|$)/);
+  return match ? match[1] : null;
+}
+
 // Add event listener for the Enter key
 videoInput.addEventListener('keypress', function(event) {
   // Check if the pressed key is Enter
   if (event.key === 'Enter') {
     // Get the input value
-    const videoId = videoInput.value.trim();
+    const textInput = videoInput.value.trim();
 
+    const videoCode = getYouTubeVideoCode(textInput)
     // Construct the new source URL
-    const newSrc = `https://www.youtube-nocookie.com/embed/${videoId}`;
+    const newSrc = `https://www.youtube-nocookie.com/embed/${videoCode}`;
 
     // Update the iframe src attribute
     videoIframe.src = newSrc;
@@ -19,3 +26,4 @@ videoInput.addEventListener('keypress', function(event) {
     videoInput.value = '';
   }
 });
+

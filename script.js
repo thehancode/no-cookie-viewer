d570467b6b3679a1693106efbd4ab412621a2b0e
thehancode <the.hancode1@gmail.com>
2024-11-12 12:01:17 -0500

    query param for dinamic link

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Get references to the input and iframe elements
 const videoInput = document.getElementById('videoInput');
-const videoIframe = document.querySelector('iframe');
 
 function getYouTubeVideoCode(url) {
   // Use a regular expression to match the last part of the URL that isn't a query parameter
@@ -14,16 +13,34 @@ videoInput.addEventListener('keypress', function(event) {
   if (event.key === 'Enter') {
     // Get the input value
     const textInput = videoInput.value.trim();
-
-    const videoCode = getYouTubeVideoCode(textInput)
-    // Construct the new source URL
-    const newSrc = `https://www.youtube-nocookie.com/embed/${videoCode}?rel=0`;
-
-    // Update the iframe src attribute
-    videoIframe.src = newSrc;
-
-    // Optional: Clear the input after updating
-    videoInput.value = '';
+    const videoCode = getYouTubeVideoCode(textInput);
+    if (videoCode) {
+      // Update the URL without reloading the page
+      //
+      history.pushState(null, '', `${window.location.pathname}?videoCode=${videoCode}`);
+      videoInput.value = '';
+      loadVideo(videoCode)
+    }
   }
 });
 
+function loadVideo(videoCode) {
+  console.log(" starting Loading video with code:", videoCode);
+  const videoIframe = document.querySelector('iframe');
+  const newSrc = `https://www.youtube-nocookie.com/embed/${videoCode}?rel=0`;
+  videoIframe.src = newSrc;
+  console.log("Loading video with code:", videoCode);
+}
+
+// Example usage: When the page loads, check if a video code is present
+function getVideoCodeFromQuery() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("videoCode");
+}
+
+window.addEventListener("load", () => {
+  const videoCode = getVideoCodeFromQuery();
+  if (videoCode) {
+    loadVideo(videoCode);
+  }
+});
